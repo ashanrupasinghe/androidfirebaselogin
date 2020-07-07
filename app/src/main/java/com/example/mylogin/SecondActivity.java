15d208a6bbc80ed689dfad94619ac576eae55792
ashanrupasinghe <ashanrupasinghe11@gmail.com>
2020-07-07 22:11:13 +0530

    menu and logout

--- app/src/main/java/com/example/mylogin/SecondActivity.java
+++ app/src/main/java/com/example/mylogin/SecondActivity.java
@@ -1,10 +1,13 @@
 package com.example.mylogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -37,5 +40,29 @@ public class SecondActivity extends AppCompatActivity {
                 startActivity(new Intent(SecondActivity.this,MainActivity.class));
             }
         });
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        Toast.makeText(SecondActivity.this,"Logout Success",Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(SecondActivity.this,MainActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+       // return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
